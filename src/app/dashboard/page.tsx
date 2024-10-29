364805ba2eff2299c56cb88ed3897c8cbdef56c1
Christine Trites <ctrites1@my.bcit.ca>
2024-10-29 15:45:10 -0700

    fix dashboard alignment

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -18,12 +18,12 @@ export default function DashboardPage() {
 
 			<div className="flex-1 flex flex-col">
 				<Header
-					headerMsg="Welcome Back, John"
+					headerMsg={`Welcome back, ${user.name}`}
 					subHeadingMsg="Here's what's happening with your projects today."
 				/>
 
-				<main className="flex-1 overflow-y-auto pt-20 px-4 sm:px-6 lg:px-8">
-					<div className="max-w-7xl mx-auto">
+				<main className="flex-1 overflow-y-auto pt-20 px-2 sm:px-2 lg:px-4 ">
+					<div className="max-w-7xl mx-1">
 						<div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
 							<Reminders />
 						</div>
