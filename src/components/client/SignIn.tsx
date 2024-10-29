37abc7726adedbd0da4f5abf1cde580860543877
adosha o <adoshao@adoshas-MacBook-Air.local>
2024-10-29 13:32:52 -0700

    signin component

--- src/components/client/SignIn.tsx
+++ src/components/client/SignIn.tsx
@@ -1,5 +1,4 @@
-"use client";
-
+import Image from 'next/image';
 import React from 'react';
 
 interface SignInModalProps {
@@ -10,50 +9,70 @@ interface SignInModalProps {
 const SignInModal: React.FC<SignInModalProps> = ({ showModal, setShowModal }) => {
   if (!showModal) return null;
 
+  const handleClickOutside = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (e.target === e.currentTarget) {
+      setShowModal(false);
+    }
+  };
+
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-      <div className="bg-caribbeanCurrant p-8 rounded-lg shadow-lg w-full max-w-md">
-        <div className="flex justify-between items-center mb-6">
-          <h2 className="text-white text-2xl font-bold">Sign In</h2>
-          <button
-            className="text-white text-xl"
-            onClick={() => setShowModal(false)}
-          >
-            &times;
-          </button>
+    <div
+      className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50"
+      onClick={handleClickOutside} 
+    >
+      <div className="bg-caribbeanCurrant p-8 py-20 rounded-lg shadow-lg w-96 max-w-md">
+        <div className="text-center mb-6">
+          <h2 className="text-white text-3xl font-bold font-">Sign In</h2>
         </div>
 
-        <form className="space-y-4">
-          <div>
-            <label className="block text-sm text-white">Email</label>
-            <input
-              type="email"
-              placeholder="Email"
-              className="w-full p-2 mt-1 bg-white rounded-md border border-gray-300"
-            />
-          </div>
-          <div>
-            <label className="block text-sm text-white">Password</label>
-            <input
-              type="password"
-              placeholder="Password"
-              className="w-full p-2 mt-1 bg-white rounded-md border border-gray-300"
-            />
-          </div>
-          <div className="flex items-center">
-            <input
-              type="checkbox"
-              id="remember-me"
-              className="w-4 h-4 text-blue-600 rounded"
-            />
-            <label htmlFor="remember-me" className="ml-2 text-white text-sm">
-              Remember Me
-            </label>
-          </div>
-        </form>
+        <div className="text-center mb-6">
+              <div className="flex justify-center space-x-4 mt-4">
+                <button className=" p-2 rounded-full">
+                  <Image src="/google-icon.svg" alt="Google" className="w-10 h-10" />
+                </button>
+                <button className=" p-2 rounded-full">
+                  <Image src="/windows-icon.svg" alt="Windows" className="w-10 h-10" />
+                </button>
+                <button className=" p-2 rounded-full">
+                  <Image src="/apple-icon.svg" alt="Apple" className="w-10 h-10" />
+                </button>
+              </div>
+            </div>
 
-        <div className="mt-6">
-          <button className="w-full bg-blue-900 text-white py-2 rounded-md hover:bg-blue-800">
+            <div className="flex items-center justify-center">
+            <form className="space-y-4 mx-auto">
+              <div>
+                <label className="block text-sm text-white">Email</label>
+                <input
+                  type="email"
+                  placeholder="Email"
+                  className="w-52 p-2 mt-1 bg-white rounded-md border border-gray-300"
+                />
+              </div>
+              <div>
+                <label className="block text-sm text-white">Password</label>
+                <input
+                  type="password"
+                  placeholder="Password"
+                  className="w-52 p-2 mt-1 bg-white rounded-md border border-gray-300"
+                />
+              </div>
+              <div className="flex items-center">
+                <input
+                  type="checkbox"
+                  id="remember-me"
+                  className="w-4 h-4 text-white-600 rounded"
+                />
+                <label htmlFor="remember-me" className="ml-2 text-white text-sm">
+                  Remember Me
+                </label>
+              </div>
+            </form>
+          </div>
+
+
+        <div className="mt-6 flex justify-center">
+          <button className="font-bold w-40 py-2 rounded-md  bg-spaceCadet text-white hover:bg-ylnMnBlue">
             SIGN IN
           </button>
         </div>
@@ -61,12 +80,12 @@ const SignInModal: React.FC<SignInModalProps> = ({ showModal, setShowModal }) =>
         <div className="text-center mt-4">
           <p className="text-white text-sm">
             Don’t have an account?{' '}
-            <a href="#" className="text-blue-400 underline">
+            <a href="/dashboard" className="text-white underline">
               Sign Up
             </a>
           </p>
           <p className="mt-2">
-            <a href="#" className="text-blue-400 underline">
+            <a href="#" className="text-white underline">
               Forgot your password?
             </a>
           </p>
@@ -77,4 +96,5 @@ const SignInModal: React.FC<SignInModalProps> = ({ showModal, setShowModal }) =>
 };
 
 export default SignInModal;
+
 
