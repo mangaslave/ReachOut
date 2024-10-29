c0ceaa550be102a8e378cbd8e00847820eeb39c7
keeles <kyleeeles@gmail.com>
2024-10-29 10:35:46 -0700

    package-lock.json merge conflict fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-
 import type {Metadata} from "next";
 import localFont from "next/font/local";
 import "./globals.css";
@@ -9,11 +8,10 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-  
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
