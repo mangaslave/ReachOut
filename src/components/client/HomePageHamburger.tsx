2a1a1657b276f7779fc538726b9d84e8e19f6f65
keeles <kyleeeles@gmail.com>
2024-11-08 17:27:59 -0800

    nav bar changes to hamburger for mobile

---
+++ src/components/client/HomePageHamburger.tsx
@@ -0,0 +1,67 @@
+import Image from "next/image";
+import React, {useState} from "react";
+import Link from "next/link";
+
+interface HamburgerModalProps {
+  showModal: boolean;
+  setShowModal: (value: boolean) => void;
+}
+
+const HamburgerModal: React.FC<HamburgerModalProps> = ({showModal, setShowModal}) => {
+  if (!showModal) return null;
+
+  const handleClickOutside = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (e.target === e.currentTarget) {
+      setShowModal(false);
+    }
+  };
+
+  return (
+    <div className="md:hidden fixed mt-24 inset-0 z-50" onClick={handleClickOutside}>
+      {showModal && (
+        <div className="bg-white rounded-lg float-right shadow-lg w-48 h-32">
+          <ul className="flex flex-col justify-between h-full">
+            <li className="flex justify-center items-center border-b border-spaceCadet">
+              <Link
+                aria-label="case managers"
+                href="/case-managers"
+                className="text-spaceCadet font-bold text-base md:hidden inline-block hover:text-caribbeanCurrant"
+              >
+                Case Managers
+              </Link>
+            </li>
+            <li className="flex justify-center items-center border-b border-spaceCadet">
+              <Link
+                aria-label="Job Seekers"
+                href="/job-seekers"
+                className="text-spaceCadet font-bold text-base md:hidden inline-block hover:text-caribbeanCurrant"
+              >
+                Job Seekers
+              </Link>
+            </li>
+            <li className="flex justify-center items-center border-b border-spaceCadet">
+              <Link
+                aria-label="Employers"
+                href="/employers"
+                className="text-spaceCadet font-bold text-base md:hidden inline-block hover:text-caribbeanCurrant"
+              >
+                Employers
+              </Link>
+            </li>
+            <li className="flex justify-center items-center border-b border-spaceCadet">
+              <Link
+                aria-label="About"
+                href="/about"
+                className="text-spaceCadet text-base font-bold md:hidden inline-block hover:text-caribbeanCurrant"
+              >
+                About
+              </Link>
+            </li>
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default HamburgerModal;
--- src/components/client/HomePageNavBar.tsx
+++ src/components/client/HomePageNavBar.tsx
@@ -4,13 +4,16 @@ import Link from "next/link";
 import logo from "../../../public/static/images/logo-new.svg";
 import {useState} from "react";
 import SignInModal from "./SignIn";
+import HamburgerModal from "./HomePageHamburger";
+import {FaBars} from "react-icons/fa";
 
 export default function NavBar() {
   const [showModal, setShowModal] = useState(false);
+  const [showHamburger, setShowHamburger] = useState(false);
 
   return (
-    <header className="bg-white flex justify-between w-full items-center flex-col max-h-40">
-      <div className="flex justify-evenly w-full py-5 flex-col sm:flex-row">
+    <header className="bg-white flex justify-between w-full items-center flex-col max-h-40 min-h-20">
+      <div className="flex justify-evenly w-full py-5 flex-row">
         <div className="flex items-center self-start py-2">
           <Link href="/">
             <Image src={logo} alt="Reach Out Logo" width={60} height={60} className="mr-2" />
@@ -18,12 +21,12 @@ export default function NavBar() {
           <h1 className="text-2xl font-bold text-black">ReachOut</h1>
         </div>
         <nav className="flex items-center self-center">
-          <ul className="flex space-x-5 items-center">
+          <ul className="flex space-x-1 sm:space-x-5 items-center">
             <li>
               <Link
                 aria-label="case managers"
                 href="/case-managers"
-                className="text-black font-bold text-sm sm:text-base hover:text-caribbeanCurrant"
+                className="text-black font-bold text-sm sm:text-base md:inline-block hidden hover:text-caribbeanCurrant"
               >
                 Case Managers
               </Link>
@@ -32,7 +35,7 @@ export default function NavBar() {
               <Link
                 aria-label="Job Seekers"
                 href="/job-seekers"
-                className="text-black font-bold text-sm sm:text-base hover:text-caribbeanCurrant"
+                className="text-black font-bold text-sm sm:text-base md:inline-block hidden hover:text-caribbeanCurrant"
               >
                 Job Seekers
               </Link>
@@ -41,7 +44,7 @@ export default function NavBar() {
               <Link
                 aria-label="Employers"
                 href="/employers"
-                className="text-black font-bold text-sm sm:text-base hover:text-caribbeanCurrant"
+                className="text-black font-bold text-sm sm:text-base md:inline-block hidden hover:text-caribbeanCurrant"
               >
                 Employers
               </Link>
@@ -50,20 +53,30 @@ export default function NavBar() {
               <Link
                 aria-label="About"
                 href="/about"
-                className="text-black text-sm sm:text-base font-bold hover:text-caribbeanCurrant"
+                className="text-black text-sm sm:text-base font-bold md:inline-block hidden hover:text-caribbeanCurrant"
               >
                 About
               </Link>
             </li>
             <li>
               <button
-                className="font-bold rounded-lg w-32 h-10 bg-spaceCadet text-white hover:bg-ylnMnBlue"
+                className="font-bold rounded-lg sm:w-32 w-24 h-10 bg-spaceCadet text-white hover:bg-ylnMnBlue"
                 onClick={() => setShowModal(true)}
               >
                 Sign In
               </button>
 
               <SignInModal showModal={showModal} setShowModal={setShowModal} />
+            </li>
+            <li>
+              <button
+                className="font-bold rounded-lg mx-4 md:hidden flex justify-center items-center h-10 text-3xl text-spaceCadet hover:bg-ylnMnBlue"
+                onClick={() => (showHamburger ? setShowHamburger(false) : setShowHamburger(true))}
+              >
+                <FaBars />
+              </button>
+
+              <HamburgerModal showModal={showHamburger} setShowModal={setShowHamburger} />
             </li>
           </ul>
         </nav>
