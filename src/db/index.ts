19b3b3213c9a63b4562499c78a542b11d48913c1
Ahmed Calif <ahmedcalif@DESKTOP-3M6LEKG>
2024-11-09 13:36:43 -0800

    feat: setup drizzle and turso config details

---
+++ src/db/index.ts
@@ -0,0 +1,17 @@
+import { createClient } from '@libsql/client';
+import { drizzle } from 'drizzle-orm/libsql';
+import { config } from 'dotenv';
+
+
+config({ path: '.env.local' });
+
+if (!process.env.DATABASE_URL || !process.env.DATABASE_AUTH_TOKEN) {
+  throw new Error('Database configuration missing');
+}
+
+const client = createClient({
+  url: process.env.DATABASE_URL,
+  authToken: process.env.DATABASE_AUTH_TOKEN,
+});
+
+export const db = drizzle(client);
