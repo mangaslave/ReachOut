6b660358aff98cd12816ee6eab668abfb340da0a
Ahmed Calif <ahmedcalif@DESKTOP-3M6LEKG>
2024-11-12 11:43:49 -0800

    feat: add the db details to .env.example

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,7 +3,7 @@ import { drizzle } from 'drizzle-orm/libsql';
 import { config } from 'dotenv';
 
 
-config({ path: '.env.local' });
+config({ path: '.env.example' });
 
 if (!process.env.DATABASE_URL || !process.env.DATABASE_AUTH_TOKEN) {
   throw new Error('Database configuration missing');
