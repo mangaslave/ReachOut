836a968148ac62ffebabcd154f8040ec9a7c7108
Ahmed Calif <ahmedcalif@DESKTOP-3M6LEKG>
2024-11-12 12:11:51 -0800

    fix: change the index.ts to point to .env.local

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,7 +3,7 @@ import { drizzle } from 'drizzle-orm/libsql';
 import { config } from 'dotenv';
 
 
-config({ path: '.env.example' });
+config({ path: '.env.local' });
 
 if (!process.env.DATABASE_URL || !process.env.DATABASE_AUTH_TOKEN) {
   throw new Error('Database configuration missing');
