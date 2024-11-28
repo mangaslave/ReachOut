85609dc0d7b11945b62f04ed77a258a2e1e18393
keeles <kyleeeles@gmail.com>
2024-11-27 19:58:26 -0800

    post-merge: fixing margin on inbox page

--- src/components/client/EmployerInboxPage.tsx
+++ src/components/client/EmployerInboxPage.tsx
@@ -1,19 +1,19 @@
 "use client";
 
-import {Sidebar} from "./SideBar";
 import {useState} from "react";
 import InboxPreview from "./InboxPreview";
 import {cn} from "@/lib/utils";
+import {EmployerSidebar} from "./EmployerSidebar";
 
-export default function Inbox({activeUser}: {activeUser: {name: string; email: string; image: string}}) {
+export default function EmployerInbox({activeUser}: {activeUser: {name: string; email: string; image: string}}) {
   const [collapsed, setCollapsed] = useState(false);
   return (
     <>
       <div className="flex h-screen overflow-hidden bg-gray-100">
-        <Sidebar user={activeUser} setCollapsed={setCollapsed} collapsed={collapsed} />
+        <EmployerSidebar user={activeUser} setCollapsed={setCollapsed} collapsed={collapsed} />
         <div className="mx-10">
           <div className={cn("flex-1 flex flex-col ml-16 transition-all duration-300", collapsed ? "ml-16" : "ml-64")}>
-            <header className="border-caribbeanCurrant border-b pb-3">
+            <header className="border-spaceCadet border-b pb-3">
               <h1 className="text-2xl font-bold mt-20">Inbox</h1>
               <p className="text-sm text-gray-600">See messages from others.</p>
             </header>
@@ -26,40 +26,38 @@ export default function Inbox({activeUser}: {activeUser: {name: string; email: s
                     className="p-2 w-80 mt-10 rounded border border-gray-500"
                   />
                 </form>
-                <div className="flex justify-evenly border-2 border-caribbeanCurrant bg-gray-200 rounded-xl mt-3 p-1 w-80">
-                  <button className="bg-caribbeanCurrant text-white w-16 text-center rounded-lg font-medium">
-                    All
-                  </button>
-                  <button className="hover:bg-caribbeanCurrant hover:text-white w-16 text-center rounded-lg hover:font-medium">
+                <div className="flex justify-evenly border-2 border-spaceCadet bg-gray-200 rounded-xl mt-3 p-1 w-80">
+                  <button className="bg-spaceCadet text-white w-16 text-center rounded-lg font-medium">All</button>
+                  <button className="hover:bg-spaceCadet hover:text-white w-16 text-center rounded-lg hover:font-medium">
                     Read
                   </button>
-                  <button className="hover:bg-caribbeanCurrant hover:text-white w-16 text-center rounded-lg hover:font-medium">
+                  <button className="hover:bg-spaceCadet hover:text-white w-16 text-center rounded-lg hover:font-medium">
                     Unread
                   </button>
                 </div>
                 <InboxPreview
                   sender="ReachOut"
-                  subject="Match Found!"
+                  subject="New Application!"
                   time="12:21PM"
-                  content="Hey Giselle! ReachOut has found a match for Leon Howard for..."
+                  content="Hey There! You have received a new application for you listing..."
                 />
-                <div className="w-10/12 h-px bg-caribbeanCurrant my-7"></div>
+                <div className="w-10/12 h-px bg-spaceCadet my-7"></div>
                 <InboxPreview
-                  sender="Samuel Robinson"
+                  sender="James Lawson"
                   subject="Application Status"
                   time="10:02AM"
-                  content="Hey! Your client Gregory Wick's resume is currently..."
+                  content="Hey! Just following up to see if my client Gregory Wick's interview was..."
                 />
-                <div className="w-10/12 h-px bg-caribbeanCurrant my-7"></div>
+                <div className="w-10/12 h-px bg-spaceCadet my-7"></div>
                 <InboxPreview
-                  sender="Sarah Duvale"
-                  subject="New Job Match"
+                  sender="Samantha McMaster"
+                  subject="Interview Reschedule"
                   time="Yesterday"
-                  content="So I've seen a couple of companies that I think want to get into..."
+                  content="Hey, apologies for the last minute schedule change. My client is..."
                 />
               </section>
               <section className="-ml-16">
-                <div className="bg-caribbeanCurrant w-px h-full"></div>
+                <div className="bg-spaceCadet w-px h-full"></div>
               </section>
               <section className="flex items-center justify-center h-screen">
                 <div className="m-72">No message selected...</div>
